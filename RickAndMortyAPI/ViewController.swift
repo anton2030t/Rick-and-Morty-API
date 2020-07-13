bec66905cdd9ff35b63ef7d09870961579494e7e
Anton Larchenko <anton2030t@gmail.com>
2020-07-13 22:19:48 +0300

    Initial Commit

---
+++ RickAndMortyAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RickAndMortyAPI
+//
+//  Created by Anton Larchenko on 13.07.2020.
+//  Copyright © 2020 Anton Larchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
